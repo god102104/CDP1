fa79c72950418cfd7d20977bdaa3cf4d8e0ecc5f
dongYoun2 <ehddbs6425@naver.com>
2020-04-26 18:52:00 +0900

    connect vue to express

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import indexRouter from "./routes/index";
+import history from "connect-history-api-fallback";
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(history());
+
+app.use("/", indexRouter);
 
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
